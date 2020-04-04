3e5cc1a174e39f18072a6b1ed1a4829f8e6e6346
r-khanas <rostyslav.khanas@binary-studio.com>
2020-04-04 15:10:47 +0300

    some logic

--- services/index.js
+++ services/index.js
@@ -1,20 +1,45 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+const convertType = (type) => {
+  switch (type.type) {
+    case "string":
+      return { type: String };
+    case "number":
+      return { type: Number };
+  }
+};
+
+const convertArray = (array, mongooseSchema) => {
+  let newArray = [convertType(array.value.items)];
+  // Object.entries(array.value.items).forEach(([key, value]) =>
+  //   newArray.push({ [key]: value })
+  // );
+  Object.assign(mongooseSchema, { [array.key]: newArray });
+  return mongooseSchema;
+};
+
+const convertObject = (properties, mongooseSchema) => {
+  let newSchema = {};
+  Object.entries(properties).forEach(([key, value]) => {
+    if (value.type === "object") {
+      convertObject({ [key]: value.properties }, newSchema);
+    } else if (value.type === "array") {
+      convertArray({ key, value }, mongooseSchema);
+    } else {
+      Object.assign(newSchema, { [key]: value });
+    }
+    Object.assign(mongooseSchema, newSchema);
+  });
+  return mongooseSchema;
+};
+
 const convertJson = (jsonSchema, mongooseSchema) => {
   if (jsonSchema.type === "object") {
-    let newSchema = {};
-    Object.entries(jsonSchema.properties).forEach(([key, value]) => {
-      console.log("KEY", key, "VALUE", value);
-      if (value.type === "object") {
-        Object.assign(newSchema, { [key]: convertJson(value, newSchema) });
-      } else {
-        Object.assign(newSchema, { [key]: value });
-      }
-    });
-    Object.assign(mongooseSchema, newSchema);
+    convertObject(jsonSchema.properties, mongooseSchema);
   } else {
-    Object.assign(mongooseSchema, jsonSchema);
+    console.log(jsonSchema);
+    Object.assign(mongooseSchema, convertType(jsonSchema));
   }
   return mongooseSchema;
 };
